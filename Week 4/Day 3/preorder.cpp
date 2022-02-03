4066fd662b90695fd3112a0d9bd1802314f1078f
Shubham Modi <shubhammodi@FVFGV65GQ05N.local>
2022-02-03 18:45:55 +0530

    Week 4 Day 4

---
+++ Week 4/Day 3/preorder.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node  
+{ 
+  int data; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      data=k;
+      left=right=NULL;
+  }
+};
+
+void preorder(Node *root){
+    if(root!=NULL){
+        cout<<root->data<<" ";
+        preorder(root->left);
+        preorder(root->right);    
+    }
+}  
+
+int main() {
+	
+	Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->right->left=new Node(40);
+	root->right->right=new Node(50);
+	
+	preorder(root);
+}
